503f9c1ee66f7655323ea2aef1bf808fe629aea7
Enhaloed <mount.james96@gmail.com>
2021-09-08 00:20:59 +1200

    Added basic move card models

--- game/models/Event.py
+++ game/models/Event.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from . import Area
 from fowsim.utils import AbstractModel
+from fowsim import constants as CONS
 
 
 class GameEvent(AbstractModel):
@@ -19,9 +20,15 @@ class GameEvent(AbstractModel):
         return cls
 
 
-class MoveCard(GameEvent):
+# Turn the values into tuples
+MOVE_CARD_POSITION_CHOICES = [(x, x) for x in CONS.MOVE_CARD_POSITION_CHOICES_VALUES]
+
+
+class MoveCardFromPosition(GameEvent):
     from_area = models.ManyToManyField(Area, related_name="from_area")
     to_area = models.ManyToManyField(Area, related_name="to_area")
+    from_position = models.TextField(choices=MOVE_CARD_POSITION_CHOICES)
+    to_position = models.TextField(choices=MOVE_CARD_POSITION_CHOICES)
 
     def __str__(self):
         return 'Moves card from %s to %s' % (self.from_area.name, self.to_area.name)

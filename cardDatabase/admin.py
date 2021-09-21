909e07b01398b31f5e423d26a2d097fefb3a6f7d
Enhaloed <mount.james96@gmail.com>
2021-09-22 01:43:09 +1200

    Add basic search features for cards

--- cardDatabase/admin.py
+++ cardDatabase/admin.py
@@ -3,7 +3,13 @@ from cardDatabase.models.Ability import *
 from cardDatabase.models.CardType import *
 from cardDatabase.models.Effects import OneTimeEffect
 
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'card_id', )
+    search_fields = ['name', 'ability_texts__text']
+
+class AbilityTextAdmin(admin.ModelAdmin):
+    list_display = ('text',)
 
 admin.site.register(OneTimeEffect)
-admin.site.register(Card)
-admin.site.register(AbilityText)
+admin.site.register(Card, CardAdmin)
+admin.site.register(AbilityText, AbilityTextAdmin)

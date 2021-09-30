351b0b07b270d21d7d1dc04283c184bb210fbc9e
Enhaloed <mount.james96@gmail.com>
2021-09-30 23:30:21 +1300

    Ability text now uses bubbles (as it should)

--- cardDatabase/templatetags/card_database_tags.py
+++ cardDatabase/templatetags/card_database_tags.py
@@ -1,14 +1,11 @@
+import re
+
 from django import template
 from django.utils.safestring import mark_safe
 from django.templatetags.static import static
 
 
 register = template.Library()
-
-
-@register.simple_tag
-def format_ability_text(text):
-    return mark_safe(text)
 
 
 ATTR_TO_FILENAMES = {
@@ -30,3 +27,23 @@ def format_cost_text(text):
 
 def attribute_to_img_html(attr):
     return '<img class="cost-img" src="%s">' % static('costs/' + ATTR_TO_FILENAMES[attr])
+
+
+def make_bubble_html(text):
+    content = text[1:-1]  # Strip '[]' from the ends
+    return '<div class="bubble-text">%s</div>' % content
+
+
+def make_bubbles(text):
+    matches = re.findall('\[[^\]]*\]', text)
+    for match in matches:
+        if '+' not in text:  # Skip [+X/+Y]
+            text = text.replace(match, make_bubble_html(match))
+    return text
+
+
+@register.simple_tag
+def format_ability_text(text):
+    text = format_cost_text(text)
+    text = make_bubbles(text)
+    return mark_safe(text)

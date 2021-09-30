4054ef968e8f3474a6566989457e0310a7c696fa
Enhaloed <mount.james96@gmail.com>
2021-09-30 22:56:06 +1300

    Card view page now uses attribute icons for cost

--- cardDatabase/templatetags/card_database_tags.py
+++ cardDatabase/templatetags/card_database_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.templatetags.static import static
 
 
 register = template.Library()
@@ -8,3 +9,24 @@ register = template.Library()
 @register.simple_tag
 def format_ability_text(text):
     return mark_safe(text)
+
+
+ATTR_TO_FILENAMES = {
+    'R': 'fire.png',
+    'B': 'darkness.png',
+    'W': 'light.png',
+    'U': 'water.png',
+    'G': 'wind.png',
+}
+
+
+@register.simple_tag
+def format_cost_text(text):
+    for attr in ATTR_TO_FILENAMES:
+        text = text.replace('{%s}' % attr, attribute_to_img_html(attr))
+
+    return mark_safe(text)
+
+
+def attribute_to_img_html(attr):
+    return '<img class="cost-img" src="%s">' % static('costs/' + ATTR_TO_FILENAMES[attr])

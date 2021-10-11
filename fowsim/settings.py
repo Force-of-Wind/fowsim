06fa7064ae2beddfba92bdda7c9dca68bd1024fd
Kossetsoup <fingyer@hotmail.com>
2021-10-11 22:13:09 +1030

    add .env for redis channel and update README.txt to README.md

--- fowsim/settings.py
+++ fowsim/settings.py
@@ -148,7 +148,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [(os.environ.get('REDIS_HOST'), os.environ.get('REDIS_PORT'))],
         },
     },
 }

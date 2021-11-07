a77811f13963f4c8b0a58080cdadb51757ec6fd5
Enhaloed <mount.james96@gmail.com>
2021-11-08 03:03:34 +1300

    Add format select buttons

--- cardDatabase/static/js/base.js
+++ cardDatabase/static/js/base.js
@@ -53,5 +53,33 @@ $(function(){
         let params = new URLSearchParams(formData);
         params.append('form_type', this.id);
         window.location.replace('/search/' + '?' + params.toString());
+    });
+
+    function select_format_sets(format_clusters, set_active){
+        for (let i = 0; i < format_clusters.length; i++){
+            let cluster = format_clusters[i];
+            for (let j = 0; j < cluster.sets.length; j++) {
+                let format_set = cluster.sets[j];
+                let set_mdb_select = $(`.sets-select .mdb-select ul span.filtrable:contains('${format_set.name.replace("'", "\\'")}')`);
+                if (set_active && !set_mdb_select.parent().hasClass('active') ||
+                    !set_active && set_mdb_select.parent().hasClass('active')){
+                    set_mdb_select.click();
+                }
+            }
+        }
+    }
+    $('#new-frontiers-select').click(function(event){
+        event.preventDefault();
+        let NF_clusters = FOWDB_SET_JSON.clusters.slice(FOWDB_SET_JSON.clusters.length - 2, FOWDB_SET_JSON.clusters.length);
+        select_format_sets(NF_clusters, true);
+    });
+    $('#wanderer-select').click(function(event){
+        event.preventDefault();
+        //TODO Remove OG Valhalla if it gets added
+        select_format_sets(FOWDB_SET_JSON.clusters, true);
+    });
+    $('#clear-all-sets').click(function(event){
+        event.preventDefault();
+        select_format_sets(FOWDB_SET_JSON.clusters, false);
     })
 });

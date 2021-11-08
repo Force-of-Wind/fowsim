cf22628f3dc2aaed5fa3eec56cda3987f7ec0cab
Kossetsoup <fingyer@hotmail.com>
2021-11-08 20:51:17 +1030

    add command to populate card_image links with existing files located in MEDIA_ROOT

---
+++ cardDatabase/management/commands/assign_existing_card_images.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.management.base import BaseCommand
+from cardDatabase.models.CardType import Card
+
+class Command(BaseCommand):
+    help = 'Associates cards in the database with images in the MEDIA_ROOT based on ID.'
+    
+    def handle(self, *args, **kwargs):
+        models = Card.objects.all()
+
+        for card in models:
+            try:
+                print(f'Trying to add image for {card.card_id}... ', end='')
+                card.card_image = os.path.join('cards', f'{card.card_id}.jpg')
+                card.save()
+                print('Success!')
+            except:
+                print('Failed, image doesn\'t exist?')

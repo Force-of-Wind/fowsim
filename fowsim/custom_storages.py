6ff58b0d7014eaab086f52f83d8272b1631cfe22
Enhaloed <mount.james96@gmail.com>
2021-11-11 22:21:35 +1300

    Add cache busting for static files

--- fowsim/custom_storages.py
+++ fowsim/custom_storages.py
@@ -1,8 +1,11 @@
 from django.conf import settings
+from django.contrib.staticfiles.storage import ManifestFilesMixin
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storage):
+
+class StaticStorage(ManifestFilesMixin, S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION

149c0ec45f6bd3579b8af9832cc5eecaa365a818
Enhaloed <mount.james96@gmail.com>
2021-12-06 16:54:55 +1300

    Better fix for image assigning script

--- cardDatabase/management/commands/assign_existing_card_images.py
+++ cardDatabase/management/commands/assign_existing_card_images.py
@@ -1,8 +1,9 @@
 import os
 
 from django.core.management.base import BaseCommand
-from cardDatabase.models.CardType import Card
+from django.core.files.storage import default_storage
 
+from cardDatabase.models.CardType import Card
 from fowsim import constants as CONS
 
 class Command(BaseCommand):
@@ -13,10 +14,16 @@ class Command(BaseCommand):
 
         for card in models:
             try:
-                print(f'Trying to add image for {card.card_id}... ', end='')
                 card_image_path = os.path.join('cards', f'{card.card_id}.jpg')
-                card.card_image = card_image_path
+                if default_storage.exists(card_image_path):
+                    card.card_image = card_image_path
+                else:
+                    second_attempt = card_image_path.replace('^', '')
+                    if default_storage.exists(second_attempt):
+                        card.card_image = second_attempt
+                    else:
+                        raise Exception(f'image for {card.set_code}: {card.name} does not exist')
                 card.save(use_resize=False)
-                print('Success!')
-            except:
+            except Exception as e:
+                print(e)
                 print('Failed, image doesn\'t exist?')

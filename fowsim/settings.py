21edbdf68f5065c835f401288693f3939f29ec5e
Enhaloed <mount.james96@gmail.com>
2021-12-07 18:23:36 +1300

    Add Email/SMTP config

--- fowsim/settings.py
+++ fowsim/settings.py
@@ -193,3 +193,10 @@ LOGGING = {
 }
 
 USER_AGENTS_CACHE = 'default'
+
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'forceofwindonline@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_PORT = 587

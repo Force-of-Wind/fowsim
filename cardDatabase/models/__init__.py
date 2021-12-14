dfd7784c6d25807e36c7e05598e6c7a874fb8d32
Enhaloed <mount.james96@gmail.com>
2021-12-15 00:12:20 +1300

    Fix missing import for Keyword

--- cardDatabase/models/__init__.py
+++ cardDatabase/models/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from importlib import import_module
 from .CardType import Card, CardColour
 from .User import Profile
+from .Ability import Keyword
 '''
 # iterate through the modules in the current package
 package_dir = Path(__file__).resolve().parent

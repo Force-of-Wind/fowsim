acceb32edba5d68aea37e218edf64820f5cf6448
Enhaloed <mount.james96@gmail.com>
2022-03-16 00:10:08 +1300

    Add registration page for mobile

--- cardDatabase/forms.py
+++ cardDatabase/forms.py
@@ -129,3 +129,10 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Password'})

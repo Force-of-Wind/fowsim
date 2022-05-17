cdc2d60a6b229573100695dcf7bd04a252490f9f
Zaloog <gramslars@gmail.com>
2022-05-17 23:20:25 +0200

    Adjust Paginator width

--- cardDatabase/views.py
+++ cardDatabase/views.py
@@ -296,7 +296,7 @@ def search_for_cards(request):
     if 'cards' in ctx:
         paginator = Paginator(ctx['cards'], request.GET.get('num_per_page', 30))
         page_number = request.GET.get('page', 1)
-        ctx['page_range'] = paginator.get_elided_page_range(number=page_number)
+        ctx['page_range'] = paginator.get_elided_page_range(number=page_number, on_each_side=1, on_ends=1)
         ctx['total_count'] = len(ctx['cards'])
         ctx['cards'] = paginator.get_page(page_number)
     return render(request, 'cardDatabase/html/search.html', context=ctx)

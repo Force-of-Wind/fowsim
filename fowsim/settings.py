e5afddb8ecf4f1e87b6649587147e70cdd3a581f
Enhaloed <mount.james96@gmail.com>
2022-10-26 01:18:19 +1300

    Remove duplicated comment

--- fowsim/settings.py
+++ fowsim/settings.py
@@ -27,8 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-# SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('PRODUCTION'):
     print('Running in PRODUCTION mode...')
     DEBUG = False

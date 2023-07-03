00d5273d5cfd2eb789e8e319575627e1d9176f28
Niebvelungen <dom.aha15@gmail.com>
2023-07-03 16:07:16 +0200

    added empty data attribute check

--- cardDatabase/static/js/user_decklists.js
+++ cardDatabase/static/js/user_decklists.js
@@ -41,6 +41,9 @@ $(function() {
 
    //localize date times
    $('.local-date-time').each(function() {
+        if(!$(this).data('epoch'))
+            return;
+
         //use epoch to increase time
         var dateTime = new Date(0); 
         dateTime.setUTCSeconds($(this).data('epoch'));

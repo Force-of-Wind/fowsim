ba854ffbbf30d8c63f074641886a4499adbe8f3d
Niebvelungen <dom.aha15@gmail.com>
2025-01-09 23:08:47 +0100

    Added db structure for tournament feature

--- cardDatabase/models/Tournament.py
+++ cardDatabase/models/Tournament.py
@@ -3,32 +3,46 @@ from fowsim import constants as CONS
 
 class Tournament(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
-    meta_data = models.JSONField(blank=False, null=False)
+    meta_data = models.JSONField(null=False)
     is_online = models.BooleanField(null=False, blank=False, default=False)
-    deck_registration_deadline = models.DateTimeField(blank=False, null=False)
-    deck_registration_locked = models.BooleanField(null=False, blank=False, default=False)
+    registration_deadline = models.DateTimeField(blank=False, null=False)
+    registration_locked = models.BooleanField(null=False, blank=False, default=False)
     deck_edit_deadline = models.DateTimeField(blank=False, null=False)
     deck_edit_locked = models.BooleanField(null=False, blank=False, default=False)
     tournament_finished = models.BooleanField(null=False, blank=False, default=False)
     reveal_decklists = models.BooleanField(null=False, blank=False, default=False)
-    #Format after PR is in
+    format = models.ForeignKey('Format', on_delete=models.SET_NULL, null=True, blank=True)
+    level = models.ForeignKey('TournamentLevel', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
     
 class TournamentPlayer(models.Model):
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', related_name='tournament_player' , on_delete=models.CASCADE)
     registration_status = models.TextField(max_length=32, blank=True, null=True, default=None,
                                  choices=CONS.TOURNAMENT_PLAYER_REGISTRATION_STATES)
-    user_data = models.JSONField(blank=False, null=False)
+    last_registration_updated_by = models.ForeignKey('Profile', on_delete=models.SET_NULL, blank=True, null=True, default=None)
+    user_data = models.JSONField(null=False)
     notes = models.CharField(max_length=500, null=False, blank=False)
     deck = models.ForeignKey('DeckList', on_delete=models.CASCADE)
     standing = models.IntegerField(blank=False, null=False)
-    dropped_out = models.JSONField(blank=False, null=False, default=False)
+    dropped_out = models.BooleanField(blank=False, null=False, default=False)
 
 
 class TournamentStaff(models.Model):
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    # permissions = models.TextField(max_length=32, blank=True, null=True, default=None,
-    #                              choices=CONS.DECK_LIST_LOCK_MODE_CHOICES)
+    tournament = models.ForeignKey('Tournament', on_delete=models.CASCADE)
+    role = models.ForeignKey('StaffRole', on_delete=models.CASCADE)
 
+
+class TournamentLevel(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False)
+    code = models.CharField(max_length=2, null=False, blank=False)
+    hint = models.CharField(max_length=200, null=False, blank=False)
+    
+
+class StaffRole(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    can_read = models.BooleanField(null=False, blank=False, default=True)
+    can_write = models.BooleanField(null=False, blank=False, default=False)
+    can_delete = models.BooleanField(null=False, blank=False, default=False)

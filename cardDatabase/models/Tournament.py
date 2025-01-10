063f78fa94d587b7f5ceaf1cb4babe1582bd3ed3
Niebvelungen <dom.aha15@gmail.com>
2025-01-10 09:39:58 +0100

    Adapted model to include nessecery fields and added new view for view code

--- cardDatabase/models/Tournament.py
+++ cardDatabase/models/Tournament.py
@@ -9,7 +9,9 @@ class Tournament(models.Model):
     registration_locked = models.BooleanField(null=False, blank=False, default=False)
     deck_edit_deadline = models.DateTimeField(blank=False, null=False)
     deck_edit_locked = models.BooleanField(null=False, blank=False, default=False)
-    tournament_finished = models.BooleanField(null=False, blank=False, default=False)
+    start_datetime = models.DateTimeField(blank=False, null=False)
+    phase = models.TextField(max_length=32, blank=True, null=True, default=CONS.TOURNAMENT_PHASE_CREATED,
+                                 choices=CONS.TOURNAMENT_PHASES)
     reveal_decklists = models.BooleanField(null=False, blank=False, default=False)
     format = models.ForeignKey('Format', on_delete=models.SET_NULL, null=True, blank=True)
     level = models.ForeignKey('TournamentLevel', on_delete=models.CASCADE)
@@ -19,6 +21,7 @@ class Tournament(models.Model):
     
 class TournamentPlayer(models.Model):
     profile = models.ForeignKey('Profile', related_name='tournament_player' , on_delete=models.CASCADE)
+    tournament = models.ForeignKey('Tournament', on_delete=models.CASCADE)
     registration_status = models.TextField(max_length=32, blank=True, null=True, default=None,
                                  choices=CONS.TOURNAMENT_PLAYER_REGISTRATION_STATES)
     last_registration_updated_by = models.ForeignKey('Profile', on_delete=models.SET_NULL, blank=True, null=True, default=None)
@@ -28,17 +31,26 @@ class TournamentPlayer(models.Model):
     standing = models.IntegerField(blank=False, null=False)
     dropped_out = models.BooleanField(blank=False, null=False, default=False)
 
+    def __str__(self):
+        return self.profile.user.username
+
 
 class TournamentStaff(models.Model):
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     tournament = models.ForeignKey('Tournament', on_delete=models.CASCADE)
     role = models.ForeignKey('StaffRole', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.profile.user.username
+
 
 class TournamentLevel(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
     code = models.CharField(max_length=2, null=False, blank=False)
     hint = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
     
 
 class StaffRole(models.Model):
@@ -46,3 +58,7 @@ class StaffRole(models.Model):
     can_read = models.BooleanField(null=False, blank=False, default=True)
     can_write = models.BooleanField(null=False, blank=False, default=False)
     can_delete = models.BooleanField(null=False, blank=False, default=False)
+    default = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.title
---
+++ cardDatabase/tournament/tournament_views.py
@@ -0,0 +1,28 @@
+import json
+import re
+import datetime
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q, F, Count
+from django.forms.fields import MultipleChoiceField
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse, Http404
+from django.urls import reverse
+
+from ..forms import SearchForm, AdvancedSearchForm, AddCardForm, UserRegistrationForm, DecklistSearchForm
+from ..models.Tournament import Tournament, TournamentLevel, TournamentPlayer, TournamentStaff, StaffRole
+from fowsim import constants as CONS
+
+
+
+def show_tournaments(request):
+    tournaments = Tournament.objects.order_by('-start_datetime').all()
+
+    return render(request, 'tournament/tournament_list.html', context={
+        "tournaments": tournaments
+    })

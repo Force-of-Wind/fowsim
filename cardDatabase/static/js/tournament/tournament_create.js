a4f46d086f0ff275f9eb99a390fcd26bacb6015b
Niebvelungen <dom.aha15@gmail.com>
2025-01-17 12:28:56 +0100

    Added Skeleton Rendering Component

--- cardDatabase/static/js/tournament/tournament_create.js
+++ cardDatabase/static/js/tournament/tournament_create.js
@@ -7,4 +7,51 @@ $(document).ready(function($) {
         minDate: "today",
         time_24hr: true
     });
+
+    $('#level').on('change', function (e) {
+        let hint = $("option:selected", this).data('hint');
+
+        if(hint?.length > 0)
+            $('.level-hint').text(hint);
+        else
+            $('.level-hint').text('');
+    });
+
+    const defaultFields = {
+        localtion: {
+            label:'Location',
+            maxlenght:'200',
+            type:'text',
+            name:'location',
+            class:'form-control'
+        },
+        prices: {
+            label:'Prices',
+            maxlenght:'200',
+            type:'text',
+            name:'prices',
+            class:'form-control'
+        },
+        pricesLink: {
+            label:'Prices Link',
+            maxlenght:'200',
+            type:'text',
+            name:'prices-link',
+            class:'form-control'
+        },
+        fee: {
+            label:'Tournament Fee',
+            maxlenght:'200',
+            type:'text',
+            name:'fee',
+            class:'form-control'
+        },
+        additionalInfo: {
+            label:'Additional Info',
+            maxlenght:'500',
+            type:'textarea',
+            name:'additional-info',
+            class:'form-control'
+        },
+    };
 });
---
+++ cardDatabase/static/js/tournament/tournament_field_renderer.js
@@ -0,0 +1,13 @@
+function renderFields(fields, edit){
+    fields.array.forEach(field => {
+        
+    });
+}
+
+function renderEditField(field){ 
+
+}
+
+function renderViewField(field){
+
+}

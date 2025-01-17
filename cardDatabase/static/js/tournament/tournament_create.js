0ec6d19b69ab2d8f41fa61db6dc795a97412ffe2
Niebvelungen <dom.aha15@gmail.com>
2025-01-17 11:48:33 +0100

    Added flatpickr and configured it to return Dates in Z format and UTC+0. Made some fixes to flatprickr in darkmode

--- cardDatabase/static/js/tournament/tournament_create.js
+++ cardDatabase/static/js/tournament/tournament_create.js
@@ -1,3 +1,10 @@
 $(document).ready(function($) {
-    
+    $(".date-time-picker").flatpickr({
+        enableTime: true,
+        altInput: true,
+        altFormat: "Y-m-d H:i",
+        dateFormat: "Z",
+        minDate: "today",
+        time_24hr: true
+    });
 });

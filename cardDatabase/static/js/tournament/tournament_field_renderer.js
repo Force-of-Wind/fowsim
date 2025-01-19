095af4e1fcda2bb294d965c9d3c7ba7dfe8f7bbf
Niebvelungen <dom.aha15@gmail.com>
2025-01-19 21:33:29 +0100

    minor js changes

--- cardDatabase/static/js/tournament/tournament_field_renderer.js
+++ cardDatabase/static/js/tournament/tournament_field_renderer.js
@@ -4,8 +4,26 @@ function renderFields(fields, edit){
     });
 }
 
-function renderEditField(field){ 
-
+function renderEditField(field) { 
+    switch (field.type) {
+        case 'text':
+            return
+            `
+                <label for="${field.name}">${field.label}</label>
+                <input id="${field.name}" maxlength="${field.maxlength}" type="text" name="${field.name}" class="${field.class}"/>
+            `;
+            break;
+        case 'textarea':
+            return
+            `
+                <label for="${field.name}">${field.label}</label>
+                <textarea id="${field.name}" maxlength="${field.maxlength}" name="${field.name}" class="${field.class}"/>
+            `;
+            break;
+    
+        default:
+            break;
+    }
 }
 
 function renderViewField(field){

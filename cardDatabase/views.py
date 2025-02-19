3a8a072a805f4f3b647a93734e9548911a506e0e
Niebvelungen <dom.aha15@gmail.com>
2025-02-19 18:16:54 +0100

    Made Many to many out of One to many and reworked check to be able to facilitate a zone in multiple formats

--- cardDatabase/views.py
+++ cardDatabase/views.py
@@ -656,7 +656,7 @@ def view_users_public(request, username=None):
 @login_required
 def create_decklist(request, format):
     decklist = DeckList.objects.create(profile=request.user.profile, name='Untitled Deck', deck_format=Format.objects.get(name=format))
-    for default_zone in DeckListZone.objects.filter(show_by_default=True, format__name=format):
+    for default_zone in DeckListZone.objects.filter(show_by_default=True, formats__name=format):
         UserDeckListZone.objects.create(zone=default_zone, position=default_zone.position, decklist=decklist)
     if request.user_agent.is_mobile or request.user_agent.is_tablet:
         return HttpResponseRedirect(reverse('cardDatabase-edit-decklist-mobile', kwargs={'decklist_id': decklist.id}))

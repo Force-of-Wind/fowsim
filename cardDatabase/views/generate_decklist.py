e803a32c05ab7b364c120ffd969592030e4d3403
Niebvelungen <dom.aha15@gmail.com>
2025-04-29 22:02:08 +0200

    Added some generate functions and upped docker file to work with the pip packages

--- cardDatabase/views/generate_decklist.py
+++ cardDatabase/views/generate_decklist.py
@@ -12,21 +12,53 @@ def get(decklist):
         other_face_list = []
         if other_faces is not None:
             for face in other_faces:
+                oracle_text = ""
+                delimiter = "\n"
+                for ability in face.abilities.order_by('position').all():
+                    oracle_text += str(ability.ability_text) + str(delimiter)
+
+                races = []
+                for race in face.races.all():
+                    races.append(race.name)
+
+                types = []
+                for type in face.types.all():
+                    types.append(type.name)
+
                 card = {
                     'id': face.card_id,
                     'name': face.name,
                     'img': face.card_image.url,
+                    'cost': face.cost,
+                    'races': races,
+                    'types': types,
+                    'ATK': face.ATK,
+                    'DEF': face.DEF,
+                    'oracleText': oracle_text
                 }
                 other_face_list.append(card)
         oracle_text = ""
         delimiter = "\n"
-        for text in deck_card.card.ability_texts.all():
-            oracle_text += str(text) + str(delimiter)
+        for ability in deck_card.card.abilities.order_by('position').all():
+            oracle_text += str(ability.ability_text) + str(delimiter)
+
+        races = []
+        for race in deck_card.card.races.all():
+            races.append(race.name)
+
+        types = []
+        for type in deck_card.card.types.all():
+            types.append(type.name)
 
         card = {
             'quantity': deck_card.quantity,
             'id': deck_card.card.card_id,
+            'cost': deck_card.card.cost,
+            'ATK': deck_card.card.ATK,
+            'DEF': deck_card.card.DEF,
             'name': deck_card.card.name,
+            'races': races,
+            'types': types,
             'zone': deck_card.zone.zone.name,
             'img': deck_card.card.card_image.url,
             'otherFaces': other_face_list,

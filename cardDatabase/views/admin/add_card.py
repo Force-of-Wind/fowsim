664613105fae57bdec32f4bf622956c8dd3ff458
Mattaiyah <7016293+Mattaiyah@users.noreply.github.com>
2025-06-28 21:39:44 -0400

    initial commit

--- cardDatabase/views/admin/add_card.py
+++ cardDatabase/views/admin/add_card.py
@@ -4,14 +4,22 @@ from django.http import HttpResponseRedirect
 
 from cardDatabase.forms import AddCardForm
 from fowsim.decorators import site_admins
+from cardDatabase.views.utils.search_context import get_set_query
 
+from cardDatabase.models.CardType import Card
+from cardDatabase.models.Spoilers import SpoilerSeason
 
 @login_required
 @site_admins
 def get(request):
     ctx = {}
     if request.method == 'GET':
-        ctx |= {'add_card_form': AddCardForm()}
+        ctx |= {'add_card_form': AddCardForm(), 'added_ids': []}
+
+        spoiler_sets = list(SpoilerSeason.objects.filter(is_active=True).values_list('set_code', flat=True))
+        if len(spoiler_sets):
+            set_query = get_set_query(spoiler_sets)
+            ctx |= {'added_ids': list(Card.objects.filter(set_query).values_list('card_id', flat=True).distinct())}
     elif request.method == 'POST':
         add_card_form = AddCardForm(request.POST, request.FILES)
         if add_card_form.is_valid():

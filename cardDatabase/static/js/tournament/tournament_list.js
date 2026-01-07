37fc87ae539333159bcb3e88aa7d5ae19482e2e8
Enhaloed <mount.james96@gmail.com>
2026-01-08 01:01:09 +1300

    Add pagination to bottom of page as well

--- cardDatabase/static/js/tournament/tournament_list.js
+++ cardDatabase/static/js/tournament/tournament_list.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('.pagination a').each(function(index){
+    $('#other-pages-bottom').html($('#other-pages-top').html());
+    $('.pagination a').each(function(index){
         let page_num = $(this).data('page-index');
         if (page_num) {
             let href = new URL(window.location.href);
